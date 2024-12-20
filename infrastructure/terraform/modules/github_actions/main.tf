7c91b9966371c52d716d1384197778bef1ddc361
Bernard Bernstein <bernard@thebernsteins.com>
2024-12-19 20:14:28 -0500

    Update terraform so it can be used for cicd workflow

--- infrastructure/terraform/modules/github_actions/main.tf
+++ infrastructure/terraform/modules/github_actions/main.tf
@@ -27,112 +27,139 @@ resource "aws_iam_role_policy" "github_actions" {
   name = "${var.project_name}-github-actions-policy-${var.environment}"
   role = aws_iam_role.github_actions.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:ListBucket",
-          "s3:DeleteObject",
-          "s3:GetBucketPolicy",
-          "s3:PutBucketPolicy",
-          "s3:CreateBucket",
-          "s3:DeleteBucket",
-          "s3:HeadBucket",
-          "s3:GetBucketVersioning",
-          "s3:PutBucketVersioning",
-          "s3:GetBucketAcl",
-          "s3:PutBucketAcl",
-          "s3:GetBucketCORS",
-          "s3:PutBucketCORS"
-        ]
-        Resource = [
-          "${var.frontend_bucket_arn}/*",
-          var.frontend_bucket_arn,
-          "arn:aws:s3:::${var.terraform_state_bucket}/*",
-          "arn:aws:s3:::${var.terraform_state_bucket}",
-          "arn:aws:s3:::${var.project_name}-station-list-${var.environment}",
-          "arn:aws:s3:::${var.project_name}-station-list-${var.environment}/*",
-          "arn:aws:s3:::${var.project_name}-cloudfront-logs-${var.environment}",
-          "arn:aws:s3:::${var.project_name}-cloudfront-logs-${var.environment}/*"
-        ]
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:DeleteItem",
-          "dynamodb:DescribeTable",
-          "dynamodb:CreateTable",
-          "dynamodb:DeleteTable",
-          "dynamodb:UpdateTable",
-          "dynamodb:ListTables"
-        ]
-        Resource = [
-          "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${var.terraform_state_lock_table}",
-          "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/stations-cache",
-          "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/harmonic-constants-cache"
-        ]
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "route53:ListHostedZones",
-          "route53:GetHostedZone",
-          "route53:ListResourceRecordSets",
-          "route53:ChangeResourceRecordSets"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "acm:RequestCertificate",
-          "acm:DescribeCertificate",
-          "acm:DeleteCertificate",
-          "acm:ListCertificates"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "cloudfront:CreateInvalidation",
-          "cloudfront:GetInvalidation",
-          "cloudfront:ListInvalidations",
-          "cloudfront:CreateDistribution",
-          "cloudfront:DeleteDistribution",
-          "cloudfront:GetDistribution",
-          "cloudfront:UpdateDistribution"
-        ]
-        Resource = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${var.cloudfront_distribution_id}"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "lambda:UpdateFunctionCode",
-          "lambda:GetFunction",
-          "lambda:CreateFunction",
-          "lambda:DeleteFunction",
-          "lambda:UpdateFunctionConfiguration"
-        ]
-        Resource = [
-          "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${var.project_name}-tides-${var.environment}",
-          "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${var.project_name}-stations-${var.environment}"
-        ]
-      }
-    ]
-  })
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:PutObject",
+            "s3:GetObject",
+            "s3:ListBucket",
+            "s3:DeleteObject",
+            "s3:GetBucketPolicy",
+            "s3:PutBucketPolicy",
+            "s3:CreateBucket",
+            "s3:DeleteBucket",
+            "s3:HeadBucket",
+            "s3:GetBucketVersioning",
+            "s3:PutBucketVersioning",
+            "s3:GetBucketAcl",
+            "s3:PutBucketAcl",
+            "s3:GetBucketCORS",
+            "s3:PutBucketCORS",
+            "s3:GetBucketWebsite",
+            "s3:PutBucketWebsite",
+            "s3:DeleteBucketWebsite",
+            "s3:GetBucketTagging",
+            "s3:PutBucketTagging",
+            "s3:GetAccelerateConfiguration",
+            "s3:GetBucketRequestPayment"
+          ],
+          "Resource" : [
+            "${var.frontend_bucket_arn}/*",
+            "${var.frontend_bucket_arn}",
+            "arn:aws:s3:::${var.terraform_state_bucket}/*",
+            "arn:aws:s3:::${var.terraform_state_bucket}",
+            "arn:aws:s3:::${var.project_name}-station-list-${var.environment}",
+            "arn:aws:s3:::${var.project_name}-station-list-${var.environment}/*",
+            "arn:aws:s3:::${var.project_name}-cloudfront-logs-${var.environment}",
+            "arn:aws:s3:::${var.project_name}-cloudfront-logs-${var.environment}/*"
+          ]
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "dynamodb:GetItem",
+            "dynamodb:PutItem",
+            "dynamodb:DeleteItem",
+            "dynamodb:DescribeTable",
+            "dynamodb:CreateTable",
+            "dynamodb:DeleteTable",
+            "dynamodb:UpdateTable",
+            "dynamodb:ListTables",
+            "dynamodb:DescribeContinuousBackups",
+            "dynamodb:UpdateContinuousBackups",
+            "dynamodb:ListTagsOfResource",
+            "dynamodb:TagResource",
+            "dynamodb:UntagResource",
+            "dynamodb:DescribeTimeToLive"
+          ],
+          "Resource" : [
+            "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${var.terraform_state_lock_table}",
+            "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/stations-cache",
+            "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/harmonic-constants-cache"
+          ]
+        },
+        {
+          "Effect" : "Allow",
+            "Action" : [
+              "route53:ListHostedZones",
+              "route53:GetHostedZone",
+              "route53:ListResourceRecordSets",
+              "route53:ChangeResourceRecordSets",
+              "route53:ListTagsForResource",
+              "route53:GetTagsForResource",
+              "route53:ChangeTagsForResource"
+            ],
+          "Resource" : "*"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "acm:RequestCertificate",
+            "acm:DescribeCertificate",
+            "acm:DeleteCertificate",
+            "acm:ListCertificates",
+            "acm:ListTagsForCertificate",
+            "acm:AddTagsToCertificate",
+            "acm:RemoveTagsFromCertificate"
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "cloudfront:CreateInvalidation",
+            "cloudfront:GetInvalidation",
+            "cloudfront:ListInvalidations",
+            "cloudfront:CreateDistribution",
+            "cloudfront:DeleteDistribution",
+            "cloudfront:GetDistribution",
+            "cloudfront:UpdateDistribution",
+            "cloudfront:TagResource",
+            "cloudfront:UntagResource",
+            "cloudfront:ListTagsForResource"
+          ],
+          "Resource" : "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${var.cloudfront_distribution_id}"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "lambda:UpdateFunctionCode",
+            "lambda:GetFunction",
+            "lambda:CreateFunction",
+            "lambda:DeleteFunction",
+            "lambda:UpdateFunctionConfiguration",
+            "lambda:ListTags",
+            "lambda:TagResource",
+            "lambda:UntagResource"
+          ],
+          "Resource" : [
+            "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${var.project_name}-tides-${var.environment}",
+            "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${var.project_name}-stations-${var.environment}"
+          ]
+        }
+      ]
+    }
+  )
 }
 
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url = "https://token.actions.githubusercontent.com"
 
-  client_id_list = ["sts.amazonaws.com"]
+  client_id_list = [ "sts.amazonaws.com" ]
 
   thumbprint_list = [
     "6938fd4d98bab03faadb97b34396831e3780aea1"
