f2c63169824ce11fa61d71360ec8fac85ff4178a
Bernard Bernstein <bernard@thebernsteins.com>
2024-12-30 10:57:02 -0500

    Fix formatting

--- infrastructure/terraform/modules/base_networking/versions.tf
+++ infrastructure/terraform/modules/base_networking/versions.tf
@@ -3,10 +3,8 @@ terraform {
 
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = "~> 5.0"
-
-      # tflint:ignore:AWS_REGION
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
       configuration_aliases = [aws.us-east-1]
     }
   }
